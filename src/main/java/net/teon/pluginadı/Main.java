561f6053cbbd574446e073014a8f9ab7629a70a5
jakobneufeld <jakob.n.neufeld@gmail.com>
2021-02-07 18:02:25 +0100

    Initial commit

---
+++ src/main/java/net/teon/pluginadı/Main.java
@@ -0,0 +1,21 @@
+package net.teon.pluginadı;
+
+import cn.nukkit.plugin.PluginBase;
+
+public class Main extends PluginBase {
+
+    @Override
+    public void onLoad() {
+
+    }
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
